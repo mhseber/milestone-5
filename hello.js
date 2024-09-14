1c2f54878f63e658b3c10cd85612e2485ae71549
mahamudul hasan seber <sebermh@gmail.com>
2024-09-14 15:28:32 +0600

    test

--- hello.js
+++ hello.js
@@ -1,1 +1,25 @@
 // lets stat to milestone-5
+
+// i love codeing 
+// i love codeing 
+// i love codeing 
+// i love codeing 
+// i love codeing 
+// i love codeing 
+// i love codeing 
+// i love codeing 
+// i love codeing 
+// i love codeing 
+// i love codeing 
+// i love codeing 
+// i love codeing 
+// i love codeing 
+
+const food = ['burger', 'pizza', 'chikan', 'nodolos']
+if (food <= 200) {
+    console.log('you Eat this')
+}
+else {
+    console.log('SORRY SIR')
+}
+
