cd5e5e095588bed32db2cfd11cf0cd3c4073a2f0
mahamudul hasan seber <sebermh@gmail.com>
2024-09-18 18:30:15 +0600

    practice time

---
+++ extra-class/js/index.js
@@ -0,0 +1,11 @@
+const head = document.getElementById('header');
+// console.log(head)
+header.style.color = "red"
+header.style.backgroundColor = "yellow"
+header.style.border = "3px solid black"
+header.style.padding = "20px"
+header.style.justify - content =  "center"
+// const all = document.getElementById('body');
+// console.log(all)
+// const ulList = document.getElementById('ul-list');
+// console.log(ulList)
