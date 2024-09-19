bcb3ae372c96503d95769963710673e329ccbe1d
mahamudul hasan seber <sebermh@gmail.com>
2024-09-19 18:40:17 +0600

    module-27

--- extra-class/js/index.js
+++ extra-class/js/index.js
@@ -4,7 +4,7 @@ header.style.color = "red"
 header.style.backgroundColor = "yellow"
 header.style.border = "3px solid black"
 header.style.padding = "20px"
-header.style.justify - content =  "center"
+
 // const all = document.getElementById('body');
 // console.log(all)
 // const ulList = document.getElementById('ul-list');
