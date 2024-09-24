24c4b14508f1b1067b8c3e57c22c59f537c6a35f
mahamudul hasan seber <sebermh@gmail.com>
2024-09-24 22:38:51 +0600

    assignment-5

---
+++ module-28/payoo-mfs-recap/js/addmoney.js
@@ -0,0 +1,24 @@
+// console.log('Every thing is ok Go')
+
+document.getElementById('btn-add-money')
+    .addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const addMoney = document.getElementById('input-add-money').value;
+        const addMoneyNumber = parseFloat(addMoney);
+        const pinNumber = document.getElementById('input-pin-number').value;
+        console.log(addMoney, pinNumber);
+
+        // wrong way to verify pin
+
+        if (pinNumber === '1234') {
+            const balance = document.getElementById('account-balance').innerText;
+            const balanceNumber = parseFloat(balance)
+
+            const newBalance = balanceNumber + addMoneyNumber;
+            document.getElementById('account-balance').innerText = newBalance;
+        }
+        else {
+            alert('failed to add money')
+        }
+    })
